1daa393c3045442e3fc26ce27871886a824d1110
sagar-mistry <sagarmistry71@gmail.com>
2024-09-07 00:08:54 +0530

    Added docker support

---
+++ Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace DockerWebApiSampleApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("get-all-users")]
+        public async Task<IActionResult> Get()
+        {
+            var users = new
+            {
+                name = "Sagar",
+                age = 30,
+                currentTimestamp = DateTime.Now.Ticks
+            };
+            return Ok(users);
+        }
+    }
+}
